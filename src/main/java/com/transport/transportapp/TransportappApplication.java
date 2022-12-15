e937734d0849808f3abb958f49e9bbcac7cd4d6d
Vitaliy Skoryy <102066708+Skyvi0@users.noreply.github.com>
2022-12-15 11:49:35 +0100

    Initial commit

---
+++ src/main/java/com/transport/transportapp/TransportappApplication.java
@@ -0,0 +1,13 @@
+package com.transport.transportapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transport/transportapp/TransportappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transport.transportapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
