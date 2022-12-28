c16a0dbca3e9d6495d6baf0127b0572cb8fe21fd
Vitaliy Skoryy <102066708+Skyvi0@users.noreply.github.com>
2022-12-28 13:52:51 +0100

    Login Page
    Works for now just plain html for testing

--- src/main/java/com/transport/transportapp/controller/LoginController.java
+++ src/main/java/com/transport/transportapp/controller/LoginController.java
@@ -1,10 +1,12 @@
 package com.transport.transportapp.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.transport.transportapp.entity.LoginRequest;
 import com.transport.transportapp.entity.User;
@@ -28,5 +30,12 @@ public class LoginController {
         }
 
         return ResponseEntity.ok("Logged in successfully.");
+    }
+
+    @GetMapping
+    public ModelAndView showLoginForm() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
     }
 }
---
+++ transport-app/src/components/login-form.vue
@@ -0,0 +1,36 @@
+<template>
+    <form @submit.prevent="login">
+      <label>Username:</label>
+      <input v-model="username" type="text" />
+      <br />
+      <label>Password:</label>
+    <input v-model="password" type="password" />
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: "",
+        password: "",
+      };
+    },
+    methods: {
+      async login() {
+        try {
+          const response = await axios.post("/api/login", {
+            username: this.username,
+            password: this.password
+          });
+          // Process the response from the API, e.g. by storing the token
+        } catch (error) {
+        // Handle errors returned by the API
+        }
+      }
+    }
+  };
+  </script>
+  
