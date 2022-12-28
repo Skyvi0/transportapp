8649f05c4e2f9e3bd0fc6439243d9208c3845fdb
Vitaliy Skoryy <102066708+Skyvi0@users.noreply.github.com>
2022-12-28 12:45:47 +0100

    New Unit Tests
    Added new Unit tests a login page and some other stuff the controller seems to be buggy or spring security kicks in and doesnt work with the get request have to look into it

--- src/main/java/com/transport/transportapp/entity/User.java
+++ src/main/java/com/transport/transportapp/entity/User.java
@@ -9,10 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
 import com.transport.transportapp.entity.UserSignupRequest;
 import org.springframework.security.core.GrantedAuthority;
-
 
 @Entity
 @Table(name = "users")
@@ -23,38 +21,47 @@ public class User {
     private String accessLevel;
     private String username;
     private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
-
- 
 
     // Getter- und Setter-Methoden für die Felder
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public String getAccessLevel() {
         return accessLevel;
     }
+
     public void setAccessLevel(String accessLevel) {
         this.accessLevel = accessLevel;
     }
+
     public Role getRole() {
         return role;
     }
+
     public void setRole(Role role) {
         this.role = role;
     }
